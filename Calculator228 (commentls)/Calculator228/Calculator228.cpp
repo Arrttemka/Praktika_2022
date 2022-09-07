a9dd169cca550356e4f9b5f927dd9d3fafd1052e
Arrttemka <arteeemka.31@gmail.com>
2022-09-07 15:54:18 +0300

    MAD

---
+++ Calculator228 (commentls)/Calculator228/Calculator228.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <map>
+
+bool Is_Digit (char symbol) {
+    if (symbol >= '0' && symbol<='9' || symbol == '.') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool Is_Operator(char symbol) {
+    if (symbol == '-' || symbol == '+' || symbol == '*' || symbol == '/') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool Is_Digit(const std::string& symbol) {
+    if (symbol[0] >= '0' && symbol[0] <='9' || symbol[0] == '-' && symbol.size()>1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+std::vector<std::string> To_Infix(const std::vector<std::string>& expression) {
+    std::map<std::string, int> priority;
+    //мапа с приоритетом операций, как в математике
+    priority["("] = 1;
+    priority[")"] = 1;
+    priority["+"] = 2;
+    priority["-"] = 2;
+    priority["*"] = 3;
+    priority["/"] = 3;
+    std::stack<std::string> st;
+    std::vector<std::string> answer;
+    // просто по алгоритму переводим в инф запись
+    for(const auto& symbol:expression) {
+        if (symbol == "(") {
+            st.push(symbol);
+        } else if (symbol == ")") {
+            while(st.top() != "(") {
+                answer.push_back(st.top());
+                st.pop();
+            }
+            st.pop();
+        } else if(Is_Digit(symbol)) {
+            answer.push_back(symbol);
+        } else {
+            while(!st.empty() && priority[st.top()] >= priority[symbol]) {
+                answer.push_back(st.top());
+                st.pop();
+            }
+            st.push(symbol);
+        }
+    }
+    while(!st.empty()) {
+        answer.push_back(st.top());
+        st.pop();
+    }
+    return answer;
+}
+
+double Calc(double a, double b, std::string operator_) {
+    if (operator_ == "+") {
+        return a + b;
+    }
+    if (operator_ == "-") {
+        return b - a;
+    }
+    if (operator_ == "*") {
+        return a * b;
+    }
+    if (operator_ == "/" && a != 0) {
+        return b / a;
+    }
+}
+
+double Calculate(std::vector<std::string> expression) {
+    // по алгоритму вычисляем выражение в инф записи
+    std::stack<double> st;
+    for (const auto& symbol: expression) {
+        if (Is_Digit(symbol)) {
+            //если в строке записано число, переводим его в double и запихиваем в стек
+            st.push(std::stod(symbol));
+        } else {
+            //если арифм операция, то выгружаем два числа из стека и делаем с ними эту операцию и запихиваем число обратно
+            double a = st.top();
+            st.pop();
+            double b = st.top();
+            st.pop();
+            st.push(Calc(a,b,symbol));
+        }
+    }
+    return st.top();
+}
+
+int main() {
+    std::string str;
+    //считываем строку
+    std::getline(std::cin, str);
+    //надо разбить str отдельно на числа и арифм операции и записать в expression
+    std::vector<std::string> expression(1);
+    //size_t
+    for (int i = 0; i < str.size()-1; ++i) {
+        //проверка для унарного минуса
+        //если i элемент -, и он стоит на первом месте или после ( или после +, -, *, /, то это унарный минус
+        if (str[i] == '-' && (i == 0 || str[i-1] == '(' || Is_Operator(str[i-1]))) {
+            //в отдельную ячейку записывать его не нужно. Он должен быть вместе с числом
+            expression[expression.size()-1] += str[i];
+            continue;
+        }
+        //добавляем i элемент в ячейку
+        expression[expression.size()-1] += str[i];
+        //если i это цифра, а следующий символ операция или скобка, то создаем новую ячейку, чтобы операция или скобка
+        //хранились в отдельной ячейке
+        if (!(Is_Digit(str[i+1]) && Is_Digit(str[i]))) {
+            expression.push_back("");
+        }
+    }
+    expression[expression.size()-1] += str[str.size()-1];
+
+//в инф запись
+    expression = To_Infix(expression);
+    //считаем
+    std::cout<<Calculate(expression);
+}
